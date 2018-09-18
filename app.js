bfc0a2205f677c3daf90727e5d5801966ead7726
Seung Hee Lee <seunghee@bu.edu>
2018-09-17 20:54:22 -0400

    bug fixed when sign up

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 var path = require('path');
 var firebase = require ('firebase');
 var router = express.Router();
-var admin = require("firebase-admin");
+//var admin = require("firebase-admin");
 var cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
 const secret = 'secret';
@@ -50,15 +50,8 @@ var emails = mongoose.model('Emails',{
     email:String
 })
    
-
 var newemail = new emails();
 
-/*
-var newtemp = setInterval(function(){
-    console.log(Math.round(temp_gen()))
-},5000);
-
-*/
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyCp_U1pcNOVN26LitgZCyiac-aIeGI_zfE",
@@ -78,25 +71,23 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 //Initialize Admin SDK
-
+/*
 var serviceAccount = require("./miniproject-35c0c-firebase-adminsdk-l7ec7-89f99871ca.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://miniproject-35c0c.firebaseio.com"
 });
-
+*/
 //Sign Up
 app.post('/registered', function(req,res){
     const txtEmail = req.body.email;
     const password = req.body.password;
-    console.log(txtEmail);
-    console.log(password);
     firebase.auth().createUserWithEmailAndPassword(txtEmail, password)
     .then(function(firbaseUser){
               var emailArray = [];      
               emails.count({},function(err, count){
-              console.log( "Number of docs: ", count );
+              
               if(count === 0){
                         newemail.email = txtEmail;
                         newemail.save().then((doc1)=>{
@@ -107,25 +98,25 @@ app.post('/registered', function(req,res){
                     
                 }
                 else{
-                        emails.find({}, function(err, users){
-                        var userMap = {};
-                        users.forEach(function(user){
-                        userMap[user.email] = user;
-                        console.log(userMap);
-                        if(txtEmail !== user)
-                        {
-                            newemail.email = txtEmail;
-                            newemail.save().then((doc1)=>{
-                            console.log('new emails added');
-                            },(e)=>{
+                    emails.find({}, function(err, users){
+                    var userMap = {};
+                    users.forEach(function(user){
+                    userMap[user.email] = user;
+                    //console.log(userMap);
+                    if(txtEmail !== user)
+                    {
+                        newemail.email = txtEmail;
+                        newemail.save().then((doc1)=>{
+                        
+                        },(e)=>{
 
-                                //console.log('err');
+                            //console.log('err');
 
-                            });    
-                        }
-                          
-                        });
-                        console.log('check out emails');
+                        });    
+                    }
+                        
+                    });
+                    console.log('check out emails');
                     })
                 }       
             });
@@ -140,17 +131,19 @@ app.post('/registered', function(req,res){
 
               
                 //console.log(times);
-                console.log('pushed info',newobj.bedroom.email);
+                //console.log('pushed info',newobj.bedroom.email);
                 
                 newobj.save().then((doc)=>{
 
-                    console.log('room info entered');
-                    console.log('info',doc);
+                    
                     
                 },(e)=>{
                     //console.log('err');
                 });
             }
+
+            var token = jwt.sign({userID:txtEmail},secret,{ expiresIn: '1h' });
+            res.cookie('token',token);
             res.redirect('/logged');
 
     })
@@ -168,7 +161,7 @@ app.post('/registered', function(req,res){
 app.post('/login', function(req,res){
     const txtEmail = req.body.email;
     const password = req.body.password;
-    console.log(txtEmail,password);
+    
     console.log('IN LOGIN');
     
     var promise = firebase.auth().signInWithEmailAndPassword(txtEmail,password);
@@ -177,17 +170,8 @@ app.post('/login', function(req,res){
         console.log('logged in')
         var token = jwt.sign({userID:txtEmail},secret,{ expiresIn: '1h' });
         res.cookie('token',token);
-        res.cookie('emailsending',txtEmail);
-            var isExpiredToken = false;
-            var dateNow = new Date();
-            console.log(token);
-            if(token.expiresIn < dateNow.getTime()/1000)
-            {
-                isExpiredToken = true;
-                console.log("Exipred");
-                res.redirect('/');
-            }
-        res.redirect('/logged')
+        //res.cookie('emailsending',txtEmail);
+        res.redirect('/logged');
     })
     .catch(e=>console.log(e))
     
@@ -208,9 +192,9 @@ app.post('/logout',function(req,res){
 
 //check if user logged in or not
 app.get('/logged',(req,res)=>{
-
+    
     console.log('token and email',req.cookies);
-    jwt.verify(req.cookies.token, 'secret', function(err, decoded) {
+    jwt.verify(req.cookies.token, 'secret', function(err , decoded) {
         // err
         if(err){
 
@@ -218,9 +202,13 @@ app.get('/logged',(req,res)=>{
             res.sendFile('index2.html',{root: path.join(__dirname,'./files/html')});
             
         }else{
+            
             console.log('confirmed');
-            obj.find({email:req.cookies.emailsending},function(err,doc){
+            console.log('decoded: ',decoded);
+            var Email = decoded.userID;
+            obj.find({Email},function(err,doc){
                 if(err){
+
                     return console.log('NO');
                 }
                 else{
